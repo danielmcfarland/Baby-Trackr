02a66261fdba638dc79fd5465b80862b93387d43
Daniel McFarland <daniel.mcfarland@me.com>
2024-01-10 22:13:42 +0000

    Initial Commit

---
+++ Baby Trackr/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Baby Trackr
+//
+//  Created by Daniel McFarland on 10/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
