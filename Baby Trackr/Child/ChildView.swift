ad6a2a1db2d53d46822fa0a70d12f1949e29870d
Daniel McFarland <daniel.mcfarland@me.com>
2024-01-12 14:17:50 +0000

    Adding Icon

--- Baby Trackr/Child/ChildView.swift
+++ Baby Trackr/Child/ChildView.swift
@@ -32,15 +32,22 @@ struct ChildView: View {
 struct ExtractedView: View {
     var body: some View {
         HStack {
-            RoundedRectangle(cornerRadius: 10.0)
-                .foregroundStyle(.accent)
-                .frame(width: 40, height: 40)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .strokeBorder(.white, lineWidth: 1)
-                        .strokeBorder(.accent.opacity(0.5), lineWidth: 1)
-                        .opacity(0.3)
-                )
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .foregroundStyle(Gradient(colors: [
+                        Color.accent.opacity(0.7),
+                        Color.accent.opacity(0.9),
+                    ]))
+                    .frame(width: 40, height: 40)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(.white, lineWidth: 1)
+                            .strokeBorder(.accent.opacity(0.5), lineWidth: 1)
+                            .opacity(0.3)
+                    )
+                
+                Image(systemName: "scalemass.fill")
+            }
             
             Text("Weight")
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
