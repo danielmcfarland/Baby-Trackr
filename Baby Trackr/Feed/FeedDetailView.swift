802006c57d1002f1e2765d35acb572df6e5397a2
Daniel McFarland <daniel.mcfarland@me.com>
2024-01-20 22:55:09 +0000

    Ability to view existing feed

--- Baby Trackr/Feed/FeedDetailView.swift
+++ Baby Trackr/Feed/FeedDetailView.swift
@@ -18,7 +18,7 @@ struct FeedDetailView: View {
     @State private var showCancelPrompt = false
     
     var body: some View {
-        NavigationStack {
+        VStack {
             Form {
                 Section {
                     DatePicker(selection: $feed.createdAt, in: ...Date(), displayedComponents: .date, label: {
@@ -39,81 +39,81 @@ struct FeedDetailView: View {
                     .foregroundStyle(Color.gray)
                 }
                 
-//                if feed.typeValue == FeedType.bottle.rawValue {
-//                    Section {
-//                        Picker("Bottle Type", selection: $feed.bottleTypeValue) {
-//                            ForEach(BottleType.allCases) { bottleType in
-//                                Text(bottleType.rawValue).tag(bottleType.rawValue)
-//                            }
-//                        }
-//                        .foregroundStyle(Color.gray)
-//                        
-//                        Picker("Bottle Size", selection: $feed.bottleSizeValue) {
-//                            ForEach(BottleSize.allCases) { bottleSize in
-//                                Text(bottleSize.rawValue).tag(bottleSize.rawValue)
-//                            }
-//                        }
-//                        .foregroundStyle(Color.gray)
-//                    }
-//                }
-//                
-//                if feed.typeValue == FeedType.breast.rawValue {
-//                    Section {
-//                        Picker("Side", selection: $feed.breastSideValue) {
-//                            ForEach(BreastSide.allCases) { breastSide in
-//                                Text(breastSide.rawValue).tag(breastSide.rawValue)
-//                            }
-//                        }
-//                        .foregroundStyle(Color.gray)
-//                    }
-//                    
-//                    HStack {
-//                        Spacer()
-//                        Text("\(humanReadableDuration())")
-//                            .font(.system(size: 75))
-//                            .fontWeight(.light)
-//                        Spacer()
-//                    }
-//                    .listRowSeparator(.hidden)
-//                    HStack {
-//                        Spacer()
-//                        Button(action: {
-//                            trackrRunning = true
-//                            if timerStartedAt == nil {
-//                                timerStartedAt = Date()
-//                            }
-//                        }) {
-//                            IconView(size: .small, icon: "play.fill")
-//                                .opacity(trackrRunning ? 0.8 : 1)
-//                        }
-//                        .disabled(trackrRunning)
-//                        
-//                        Button(action: {
-//                            trackrRunning = false
-//                            feed.duration = feed.duration + currentDuration
-//                            timerStartedAt = nil
-//                            currentDuration = 0
-//                        }) {
-//                            IconView(size: .small, icon: "pause.fill")
-//                                .opacity(trackrRunning ? 1 : 0.8)
-//                        }
-//                        .disabled(!trackrRunning)
-//                        Spacer()
-//                    }
-//                    .listRowSeparator(.hidden)
-//                }
+                if feed.typeValue == FeedType.bottle.rawValue {
+                    Section {
+                        Picker("Bottle Type", selection: $feed.bottleTypeValue) {
+                            ForEach(BottleType.allCases) { bottleType in
+                                Text(bottleType.rawValue).tag(bottleType.rawValue)
+                            }
+                        }
+                        .foregroundStyle(Color.gray)
+                        
+                        Picker("Bottle Size", selection: $feed.bottleSizeValue) {
+                            ForEach(BottleSize.allCases) { bottleSize in
+                                Text(bottleSize.rawValue).tag(bottleSize.rawValue)
+                            }
+                        }
+                        .foregroundStyle(Color.gray)
+                    }
+                }
+                
+                if feed.typeValue == FeedType.breast.rawValue {
+                    Section {
+                        Picker("Side", selection: $feed.breastSideValue) {
+                            ForEach(BreastSide.allCases) { breastSide in
+                                Text(breastSide.rawValue).tag(breastSide.rawValue)
+                            }
+                        }
+                        .foregroundStyle(Color.gray)
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        Text("\(humanReadableDuration())")
+                            .font(.system(size: 75))
+                            .fontWeight(.light)
+                        Spacer()
+                    }
+                    .listRowSeparator(.hidden)
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            trackrRunning = true
+                            if timerStartedAt == nil {
+                                timerStartedAt = Date()
+                            }
+                        }) {
+                            IconView(size: .small, icon: "play.fill")
+                                .opacity(trackrRunning ? 0.8 : 1)
+                        }
+                        .disabled(trackrRunning)
+                        
+                        Button(action: {
+                            trackrRunning = false
+                            feed.duration = feed.duration + currentDuration
+                            timerStartedAt = nil
+                            currentDuration = 0
+                        }) {
+                            IconView(size: .small, icon: "pause.fill")
+                                .opacity(trackrRunning ? 1 : 0.8)
+                        }
+                        .disabled(!trackrRunning)
+                        Spacer()
+                    }
+                    .listRowSeparator(.hidden)
+                }
                 
             }
             .navigationTitle("Feed")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        cancelFeed()
-                    }) {
-                        Text("Cancel")
-                    }
-                }
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    Button(action: {
+//                        cancelFeed()
+//                    }) {
+//                        Text("Cancel")
+//                    }
+//                }
                 
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
@@ -127,22 +127,22 @@ struct FeedDetailView: View {
 //            .onAppear {
 //                focusedField = .measurement
 //            }
-//            .onReceive(trackr.timer) { firedDate in
-//                if !trackrRunning {
-//                    return
-//                }
-//                guard let startTime = timerStartedAt else {
-//                    return
-//                }
-//                
-//                currentDuration = Int(firedDate.timeIntervalSince(startTime))
-//            }
-//            .confirmationDialog("Cancel Feed", isPresented: $showCancelPrompt) {
-//                Button("Yes", action: dismiss.callAsFunction)
-//                Button("No", role: .cancel) { }
-//            } message: {
-//                Text("Are you sure you want to cancel this feed without saving?")
-//            }
+            .onReceive(trackr.timer) { firedDate in
+                if !trackrRunning {
+                    return
+                }
+                guard let startTime = timerStartedAt else {
+                    return
+                }
+                
+                currentDuration = Int(firedDate.timeIntervalSince(startTime))
+            }
+            .confirmationDialog("Cancel Feed", isPresented: $showCancelPrompt) {
+                Button("Yes", action: dismiss.callAsFunction)
+                Button("No", role: .cancel) { }
+            } message: {
+                Text("Are you sure you want to cancel this feed without saving?")
+            }
         }
     }
     
@@ -180,6 +180,13 @@ struct FeedDetailView: View {
 //            modelContext.insert(feed)
 //            child.feeds?.append(feed)
 //            dismiss()
+            if modelContext.hasChanges {
+                do {
+                    try modelContext.save()
+                } catch {
+                    print(error)
+                }
+            }
         }
     }
 }
